8a9b271306e344c1cf3e0973b8ac253ddfe6b6e9
WhiteHeadbanger <white.seba@hotmail.com>
2023-08-12 08:44:25 -0300

    Update colors
    Colors are now dracula colors

--- syntax_highlight/colors.py
+++ syntax_highlight/colors.py
@@ -1,58 +1,70 @@
+HEX_COLORS = {
+    'red': '#ff5555',
+    'yellow': '#f1fa8c',
+    'purple': '#bd93f9',
+    'pink': '#ff79c6',
+    'orange': '#ffb86c',
+    'green': '#50fa7b',
+    'cyan': '#8be9fd',
+    'comment': '#6272a4',
+    'foreground': '#f8f8f2'
+}
+
 COLORS = {
-    'KEYWORD': '#c11af0',
-    'NAME': '#ffffff',
-    'NUMBER': '#6a0ced',
-    'STRING': '#e7f520',
-    'LPAR': '#e88c0c',
-    'RPAR': '#e88c0c',
-    'LSQB': '#e88c0c', #LEFT SQUARE BRACKET
-    'RSQB': '#e88c0c', #RIGHT SQUARE BRACKET
-    'COLON': '#FFFFFF',
-    'COMMA': '#FFFFFF',
-    'SEMI': '#FFFFFF',
-    'PLUS': '#e8290c',
-    'MINUS': '#e8290c',
-    'STAR': '#e8290c',
-    'SLASH': '#e8290c',
-    'VBAR': '#e8290c',
-    'AMPER': '#e8290c',
-    'LESS': '#e8290c',
-    'GREATER': '#e8290c',
-    'EQUAL': '#e8290c',
-    'DOT': '#FFFFFF',
-    'PERCENT': '#e8290c',
-    'LBRACE': '#e88c0c', #LEFT CURLY
-    'RBRACE': '#e88c0c', #RIGHT CURLY
-    'EQEQUAL': '#e8290c', # ==
-    'NOTEQUAL': '#e8290c',
-    'LESSEQUAL': '#e8290c',
-    'GREATEREQUAL': '#e8290c',
-    'TILDE': '#e8290c', # ~
-    'CIRCUMFLEX': '#e8290c', # ^
-    'LEFTSHIFT': '#e8290c', # <<
-    'RIGHTSHIFT': '#e8290c', # >>
-    'DOUBLESTAR': '#e8290c',
-    'PLUSEQUAL': '#e8290c',
-    'MINEQUAL': '#e8290c',
-    'STAREQUAL': '#e8290c',
-    'SLASHEQUAL': '#e8290c',
-    'PERCENTEQUAL': '#e8290c',
-    'AMPEREQUAL': '#e8290c',
-    'VBAREQUAL': '#e8290c',
-    'CIRCUMFLEXEQUAL': '#e8290c',
-    'LEFTSHIFTEQUAL': '#e8290c',
-    'RIGHTSHIFTEQUAL': '#e8290c',
-    'DOUBLESLASH': '#e8290c',
-    'DOUBLESLASHEQUAL': '#e8290c',
-    'AT': '#e8290c',
-    'ATEQUAL': '#e8290c',
-    'RARROW': '#e8290c',
-    'ELLIPSIS': '#e8290c',
-    'COLONEQUAL': '#e8290c',
-    'OP': '#e8290c',
-    'TYPE_COMMENT': '#949693',
-    'COMMENT': '#949693',
-    'CLASS_NAME': '#0ca2e8',
-    'FUNC_NAME': '#34e80c'
+    'KEYWORD': HEX_COLORS['pink'],
+    'NAME': HEX_COLORS['foreground'],
+    'NUMBER': HEX_COLORS['purple'],
+    'STRING': HEX_COLORS['yellow'],
+    'LPAR': HEX_COLORS['foreground'],
+    'RPAR': HEX_COLORS['foreground'],
+    'LSQB': HEX_COLORS['foreground'], #LEFT SQUARE BRACKET
+    'RSQB': HEX_COLORS['foreground'], #RIGHT SQUARE BRACKET
+    'COLON': HEX_COLORS['foreground'],
+    'COMMA': HEX_COLORS['foreground'],
+    'SEMI': HEX_COLORS['foreground'],
+    'PLUS': HEX_COLORS['red'],
+    'MINUS': HEX_COLORS['red'],
+    'STAR': HEX_COLORS['red'],
+    'SLASH': HEX_COLORS['red'],
+    'VBAR': HEX_COLORS['red'],
+    'AMPER': HEX_COLORS['red'],
+    'LESS': HEX_COLORS['red'],
+    'GREATER': HEX_COLORS['red'],
+    'EQUAL': HEX_COLORS['red'],
+    'DOT': HEX_COLORS['foreground'],
+    'PERCENT': HEX_COLORS['red'],
+    'LBRACE': HEX_COLORS['foreground'], #LEFT CURLY
+    'RBRACE': HEX_COLORS['foreground'], #RIGHT CURLY
+    'EQEQUAL': HEX_COLORS['red'], # ==
+    'NOTEQUAL': HEX_COLORS['red'],
+    'LESSEQUAL': HEX_COLORS['red'],
+    'GREATEREQUAL': HEX_COLORS['red'],
+    'TILDE': HEX_COLORS['red'], # ~
+    'CIRCUMFLEX': HEX_COLORS['red'], # ^
+    'LEFTSHIFT': HEX_COLORS['red'], # <<
+    'RIGHTSHIFT': HEX_COLORS['red'], # >>
+    'DOUBLESTAR': HEX_COLORS['red'],
+    'PLUSEQUAL': HEX_COLORS['red'],
+    'MINEQUAL': HEX_COLORS['red'],
+    'STAREQUAL': HEX_COLORS['red'],
+    'SLASHEQUAL': HEX_COLORS['red'],
+    'PERCENTEQUAL': HEX_COLORS['red'],
+    'AMPEREQUAL': HEX_COLORS['red'],
+    'VBAREQUAL': HEX_COLORS['red'],
+    'CIRCUMFLEXEQUAL': HEX_COLORS['red'],
+    'LEFTSHIFTEQUAL': HEX_COLORS['red'],
+    'RIGHTSHIFTEQUAL': HEX_COLORS['red'],
+    'DOUBLESLASH': HEX_COLORS['red'],
+    'DOUBLESLASHEQUAL': HEX_COLORS['red'],
+    'AT': HEX_COLORS['red'],
+    'ATEQUAL': HEX_COLORS['red'],
+    'RARROW': HEX_COLORS['red'],
+    'ELLIPSIS': HEX_COLORS['red'],
+    'COLONEQUAL': HEX_COLORS['red'],
+    'OP': HEX_COLORS['red'],
+    'TYPE_COMMENT': HEX_COLORS['comment'],
+    'COMMENT': HEX_COLORS['comment'],
+    'CLASS_NAME': HEX_COLORS['cyan'],
+    'FUNC_NAME': HEX_COLORS['green']
 
 }
